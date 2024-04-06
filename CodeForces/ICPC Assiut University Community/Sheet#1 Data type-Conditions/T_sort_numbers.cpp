ff2f8aab1e17e5ed335b3a25b8db9081a4b1e78e
Ali Ashraf <125685096+Ashraf1551@users.noreply.github.com>
2024-04-07 01:55:25 +0600

    T_sort_numbers.cpp

--- CodeForces/ICPC Assiut University Community/Sheet#1 Data type-Conditions/T_sort_numbers.cpp
+++ CodeForces/ICPC Assiut University Community/Sheet#1 Data type-Conditions/T_sort_numbers.cpp
@@ -4,36 +4,40 @@ using namespace std;
 #define mod 1000000007
 void solve()
 {
-	int a, b, c,min,med,max;
-	cin>>a>>b>>c;
+    int a, b, c, min, med, max;
+    cin >> a >> b >> c;
 
     // Determine the maximum
-    if (a > b) {
+    if (a > b)
+    {
         if (a > c)
             max = a;
         else
             max = c;
-    } else {
+    }
+    else
+    {
         if (b > c)
             max = b;
         else
             max = c;
     }
 
-
     // Determine the minimum
-    if (a < b) {
+    if (a < b)
+    {
         if (a < c)
             min = a;
         else
             min = c;
-    } else {
+    }
+    else
+    {
         if (b < c)
             min = b;
         else
             min = c;
     }
-
 
     // Determine the medium
     if ((a >= b && a <= c) || (a >= c && a <= b))
@@ -43,21 +47,29 @@ void solve()
     else
         med = c;
 
-
-    cout<<""<<min<<endl<<""<<med<<endl<<""<<max<<endl<<endl<<""<<a<<endl<<""<<b<<endl<<""<<c<<endl;
+    cout << "" << min << endl
+         << "" << med << endl
+         << "" << max << endl
+         << endl
+         << "" << a << endl
+         << "" << b << endl
+         << "" << c << endl;
 }
-int main() {
-	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 #ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 #endif
 
-	ll test=1;
-//cin>>test;
-	while(test--)
-	{
-		solve();
-	}
-	return 0;
+    ll test = 1;
+    // cin>>test;
+    while (test--)
+    {
+        solve();
+    }
+    return 0;
 }
