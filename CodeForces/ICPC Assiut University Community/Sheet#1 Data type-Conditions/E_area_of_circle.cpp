ed47b6b6a4167d907ec802b1577a142aee9161ff
Ali Ashraf <125685096+Ashraf1551@users.noreply.github.com>
2024-04-07 01:46:56 +0600

    E_area_of_circle.cpp

--- CodeForces/ICPC Assiut University Community/Sheet#1 Data type-Conditions/E_area_of_circle.cpp
+++ CodeForces/ICPC Assiut University Community/Sheet#1 Data type-Conditions/E_area_of_circle.cpp
@@ -4,21 +4,24 @@ using namespace std;
 #define mod 1000000007
 void solve()
 {
-	double r, pi=3.141592653;
-    
-    cin>>r;
-    cout<<fixed<<setprecision(9)<<pi*r*r;
+	double r, pi = 3.141592653;
+
+	cin >> r;
+	cout << fixed << setprecision(9) << pi * r * r;
 }
-int main() {
-	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 #ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 #endif
 
-	ll test=1;
-//cin>>test;
-	while(test--)
+	ll test = 1;
+	// cin>>test;
+	while (test--)
 	{
 		solve();
 	}
