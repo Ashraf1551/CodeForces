c7e7a16d0370add43fca1061055e23eabd273a66
Ali Ashraf <125685096+Ashraf1551@users.noreply.github.com>
2024-04-07 01:48:13 +0600

    F_digits_summation.cpp

--- CodeForces/ICPC Assiut University Community/Sheet#1 Data type-Conditions/F_digits_summation.cpp
+++ CodeForces/ICPC Assiut University Community/Sheet#1 Data type-Conditions/F_digits_summation.cpp
@@ -6,21 +6,23 @@ void solve()
 {
 	long long int a, b;
 
-    cin>>a>>b;
-    long long int sum=a%10 + b%10;
-    cout<<""<<sum;
-	
+	cin >> a >> b;
+	long long int sum = a % 10 + b % 10;
+	cout << "" << sum;
 }
-int main() {
-	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 #ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 #endif
 
-	ll test=1;
-//cin>>test;
-	while(test--)
+	ll test = 1;
+	// cin>>test;
+	while (test--)
 	{
 		solve();
 	}
