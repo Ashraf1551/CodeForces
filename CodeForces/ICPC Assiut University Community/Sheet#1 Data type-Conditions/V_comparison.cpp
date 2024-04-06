ae81272c6f7d4d725f94226a5f532f31add2a7ce
Ali Ashraf <125685096+Ashraf1551@users.noreply.github.com>
2024-04-07 01:53:48 +0600

    V_comparison.cpp

--- CodeForces/ICPC Assiut University Community/Sheet#1 Data type-Conditions/V_comparison.cpp
+++ CodeForces/ICPC Assiut University Community/Sheet#1 Data type-Conditions/V_comparison.cpp
@@ -4,40 +4,43 @@ using namespace std;
 #define mod 1000000007
 void solve()
 {
-	int a,b;
+	int a, b;
 	char s;
-	cin>>a>>s>>b;
+	cin >> a >> s >> b;
 
-	if(s == '>' || s == '<')
+	if (s == '>' || s == '<')
 	{
-    if(s == '>' && a > b)
-        cout << "Right" << endl;
-    else if(s == '<' && a < b)
-        cout << "Right" << endl;
-    else
-        cout << "Wrong" << endl;
+		if (s == '>' && a > b)
+			cout << "Right" << endl;
+		else if (s == '<' && a < b)
+			cout << "Right" << endl;
+		else
+			cout << "Wrong" << endl;
 	}
 	else
 	{
-		if(s=='=')
+		if (s == '=')
 		{
-			if(a==b)
-				cout<<"Right"<<endl;
+			if (a == b)
+				cout << "Right" << endl;
 			else
-				cout<<"Wrong"<<endl;
+				cout << "Wrong" << endl;
 		}
 	}
 }
-int main() {
-	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 #ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 #endif
 
-	ll test=1;
-	//cin>>test;
-	while(test--)
+	ll test = 1;
+	// cin>>test;
+	while (test--)
 	{
 		solve();
 	}
